7c256a390b4b69a8133d82981c3e9ccfdeed016c
Carlos Vasquez <carlos@cobranix.com>
2018-08-13 02:01:56 -0700

    Switch to gorilla mux router. Updated CSS

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"regexp"
 )
 
 func main() {
@@ -13,14 +15,27 @@ func main() {
 	httpPort := flag.String("port", "8080", "HTTP Listening Address")
 	flag.Parse()
 
-	// Initialize file server
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
+	log.Println("Starting Server")
 
-	// Start the server and log any errors
-	log.Println("HTTP server started on port:", *httpPort)
+	r := mux.NewRouter()
+	r.PathPrefix("/").HandlerFunc(FileServer)
+	http.Handle("/", r)
+
+	log.Println("Listening on port: ", *httpPort)
 	err := http.ListenAndServe(":"+*httpPort, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+// Serve web files in public directory
+func FileServer(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	extension, _ := regexp.MatchString("\\.+[a-zA-Z]+", r.URL.EscapedPath())
+	// If the url contains an extension, use file server
+	if extension {
+		http.FileServer(http.Dir("./public/")).ServeHTTP(w, r)
+	} else {
+		http.ServeFile(w, r, "./public/index.html")
 	}
 }
